b086abd7c12364545343f31743ff247e56cdbb91
Ross Wilson <Ross Wilson@localhost>
2012-07-15 11:41:46 +1000

    Added more tests for 'standard' dictionary methods.

--- pycacheback/test_pycacheback.py
+++ pycacheback/test_pycacheback.py
@@ -26,8 +26,87 @@ class TestpyCacheBack(unittest.TestCase):
         self.assertRaises(KeyError, a.__getitem__, 2)
 
         # make sure the len() function works correctly
-        msg = "len(a) should be 1, got %d!" % len(a)
+        msg = 'len(a) should be 1, got %d!' % len(a)
         self.assertEqual(len(a), 1, msg)
+
+    def testDict(self):
+        """Test the dictionary methods that should still be working."""
+
+        # make an extended dictionary and define some key/value pairs
+        a = pyCacheBack()
+        a[1] = 'one'
+        a[2] = '2'
+        a[3] = 3
+        a['4'] = 'four'
+
+        # check other dictionary methods
+        msg = 'len(a) should be 4, got %d!' % len(a)
+        self.assertEqual(len(a), 4, msg)
+
+        msg = "'1 in a' was False, should be True!"
+        self.assertTrue(1 in a, msg)
+
+        msg = "'\'4\' in a' was False, should be True!"
+        self.assertTrue('4' in a, msg)
+
+        b = a.copy()
+        msg = "a.copy() doesn't return a true copy'"
+        self.assertEqual(a, b, msg)
+
+        msg = "a.get(1) should return 'one', got %s" % a.get(1)
+        self.assertEqual(a.get(1), 'one', msg)
+
+        msg = "a.get(10, 'NONE') should return 'NONE', got %s" % str(a.get(10, 'NONE'))
+        self.assertEqual(a.get(10, 'NONE'), 'NONE', msg)
+
+        msg = "a.has_key(2) should return True, got %s" % str(a.has_key(2))
+        self.assertTrue(a.has_key(2), msg)
+
+        msg = "a.has_key(10) should return False, got %s" % str(a.has_key(10))
+        self.assertFalse(a.has_key(10), msg)
+
+        msg = "a.items() should return [(1, 'one'), (2, '2'), (3, 3), ('4', 'four')], got %s" % str(a.items())
+        self.assertEqual([(1, 'one'), (2, '2'), (3, 3), ('4', 'four')], a.items(), msg)
+
+        msg = "a.keys() should return [1, 2, 3, '4'], got %s" % str(a.keys())
+        self.assertEqual([1, 2, 3, '4'], a.keys(), msg)
+
+        msg = "a.keys() should return [1, 2, 3, '4'], got %s" % str(a.keys())
+        self.assertEqual([1, 2, 3, '4'], a.keys(), msg)
+
+        msg = "a.values() should return ['one', '2', 3, 'four'], got %s" % str(a.values())
+        self.assertEqual(['one', '2', 3, 'four'], a.values(), msg)
+
+        result = a.setdefault(10, 'TEN')
+        msg = "a.setdefault(10, 'TEN') doesn't return 'TEN'?"
+        self.assertEqual(result, 'TEN', msg)
+        msg = "a.setdefault() doesn't set the default?"
+        self.assertEqual(a[10], 'TEN', msg)
+
+        result = a.pop(10)
+        msg = "a.pop(10) should return 'TEN' but got %s?" % result
+        self.assertEqual(result, 'TEN', msg)
+
+        result = a.pop(10, 'not found')
+        msg = "a.pop(10, 'not found') should return 'not found' but got %s?" % result
+        self.assertEqual(result, 'not found', msg)
+
+        #msg = "a.pop(10) should raise KeyError exception, but didn't?"
+        self.assertRaises(KeyError, a.pop, 10)
+
+        msg = "a.update(b) should set 'TEN' key but didn't"
+        b = {'TEN': 10}
+        a.update(b)
+        self.assertEqual(a['TEN'], 10, msg)
+
+        a.clear()
+        msg = 'After clear(), len(a) should be 0, got %d!' % len(a)
+        self.assertEqual(len(a), 0, msg)
+
+        b = {'TEN': 10}
+        a.update(b)
+        msg = "a.keys() should return ['TEN'], got %s" % str(a.keys())
+        self.assertEqual(a.keys(), ['TEN'], msg)
 
     def testLRU(self):
         """Test the LRU mechanism."""
@@ -36,6 +115,7 @@ class TestpyCacheBack(unittest.TestCase):
         a = pyCacheBack(2)
 
         # the LRU list should be empty when we start
+
         msg = ("Initial LRU list should be empty, but it's %s"
                % str(a._lru_list))
         self.assertEqual(a._lru_list, [], msg)

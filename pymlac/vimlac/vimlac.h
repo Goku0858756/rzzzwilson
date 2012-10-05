f620b07d11d517c8583cc701e6da83f21bf3c39e
Ross Wilson <Ross Wilson@localhost>
2012-10-05 21:53:09 +0930

    Initial commit of the C emulator.

---
+++ pymlac/vimlac/vimlac.h
@@ -0,0 +1,18 @@
+/*
+ * Interface for the vimlac emulator.
+ */
+
+#ifndef VIMLAC_H
+#define VIMLAC_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <errno.h>
+
+typedef unsigned int	WORD;
+typedef unsigned char	BYTE;
+
+#define CPU_HERZ	1800000
+
+#endif

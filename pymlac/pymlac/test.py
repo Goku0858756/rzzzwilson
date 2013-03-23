bde02abf0be34e0e3a63e88d65a5f233b673c3ff
Ross Wilson <Ross Wilson@localhost>
2013-03-23 15:42:29 +1030

    Changes.

--- pymlac/pymlac/test.py
+++ pymlac/pymlac/test.py
@@ -8,16 +8,16 @@ import wx
 
 WIDTH_SCREEN = 1024
 HEIGHT_SCREEN = 1024
-WIDTH_CONSOLE = 330 # 400 # 256
+WIDTH_CONSOLE = 400 # 256
 HEIGHT_CONSOLE = HEIGHT_SCREEN
 
 SCREEN_COLOUR = (0, 0, 0)
 CONSOLE_COLOUR = (223, 223, 169)
-PHOSPHOR_COLOUR = '#F0F000'	# yellow
-#PHOSPHOR_COLOUR = '#40FF40'	# green
+#PHOSPHOR_COLOUR = '#F0F000'	# yellow
+PHOSPHOR_COLOUR = '#40FF40'	# green
 
-V_MARGIN = 20
-CTL_MARGIN = 15
+L_AC_MARGIN = 30
+CTL_MARGIN = 10 # 38 # 15
 LED_MARGIN = 5
 
 IMAGE_LED_OFF = 'images/led_off.png'
@@ -27,13 +27,15 @@ count = 1
 
 HALF_SCREEN = HEIGHT_SCREEN/2
 
+
 class ImlacCpuThread(threading.Thread):
 
-    def __init__(self, notify_window, queue):
+    def __init__(self, report_win, queue):
 
         threading.Thread.__init__(self)
-        self._notify_window = notify_window
+        self.report_win = report_win
         self.queue = queue
+        self.running = True
         self.start()
 
     def run(self):
@@ -47,7 +49,7 @@ class ImlacCpuThread(threading.Thread):
         bottom_offset = HEIGHT_SCREEN - 1
         shrink = True
 
-        while True:
+        while self.running:
             time.sleep(0.005)
             if shrink:
                 left_offset += 1
@@ -67,24 +69,18 @@ class ImlacCpuThread(threading.Thread):
                          (right_offset, top_offset, right_offset, bottom_offset),
                          (left_offset, bottom_offset, right_offset, bottom_offset),
                          (left_offset, top_offset, left_offset, bottom_offset),
-                         (left_offset, int((bottom_offset-top_offset)/2),
-                          int((right_offset-left_offset)/2), top_offset),
-                         (int((right_offset-left_offset)/2), top_offset,
-                          right_offset, int((bottom_offset-top_offset)/2)),
-                         (right_offset, int((bottom_offset-top_offset)/2),
-                          int((right_offset-left_offset)/2), bottom_offset),
-                         (int((right_offset-left_offset)/2), bottom_offset,
-                          left_offset, int((bottom_offset-top_offset)/2))
-                        )
-
+                         (left_offset, 511, 511, top_offset),
+                         (511, top_offset, right_offset, 511),
+                         (right_offset, 511, 511, bottom_offset),
+                         (511, bottom_offset, left_offset, 511))
             self.queue.append(draw_list)
-            #print(str(draw_list))
 
     def abort(self):
         """abort CPU thread."""
 
         # Method for use by main thread to signal an abort
-        self._want_abort = 1
+        self.running = False
+        self.join()
 
 
 class Led_1(object):
@@ -110,19 +106,29 @@ class Led_16(object):
         y += 15
         self.leds = []
         mark_count = 2
-        ticks = [(x-17+led_width,y+led_height/2+5)]
-        dc = wx.PaintDC(parent)
+        self.ticks = [(x-17+led_width, y+led_height/2+5,
+                       x-17+led_width, y+led_height/2+10)]
         for i in range(16):
-            wx.StaticBitmap(parent, -1, off, pos=(x-1+i*17, y))
+            #wx.StaticBitmap(parent, -1, off, pos=(x-1+i*17, y))
             led = wx.StaticBitmap(parent, -1, on, pos=(x-1+i*17, y))
             self.leds.append(led)
             mark_count += 1
             if mark_count >= 3:
                 mark_count = 0
-                ticks.append((x+i*17 + led_width, y+led_height/2+5))
+                self.ticks.append((x+i*17 + led_width, y+led_height/2+5,
+                                   x+i*17 + led_width, y+led_height/2+10))
+
+        first = self.ticks[0]
+        last = self.ticks[-1]
+        (fx1, fy1, fx2, fy2) = first
+        (lx1, ly1, lx2, ly2) = last
+        self.ticks.append((fx1, ly1+5, lx1, ly1+5))
 
         self.set_value(0)
-        self.ticks = ticks
+
+    def paint(self, dc):
+        dc.SetPen(wx.Pen('black', 1))
+        dc.DrawLineList(self.ticks)
 
     def set_value(self, value):
         mask = 0x8000
@@ -142,38 +148,10 @@ class MyFrame(wx.Frame):
                                      size=(WIDTH_SCREEN, HEIGHT_SCREEN),
                                      pos=(0,0)) 
         self.panel_Screen.SetBackgroundColour(SCREEN_COLOUR)
-        self.panel_Screen.Bind(wx.EVT_PAINT, self.on_paint) 
         self.panel_Console = wx.Panel(self, style=wx.SIMPLE_BORDER,
                                       size=(WIDTH_CONSOLE, HEIGHT_SCREEN),
                                       pos=(WIDTH_SCREEN,0)) 
         self.panel_Console.SetBackgroundColour(CONSOLE_COLOUR)
-        self.panel_Console.Bind(wx.EVT_PAINT, self.on_paint) 
-
-        python_png = wx.Image('images/PythonPowered.png',
-                              wx.BITMAP_TYPE_PNG).ConvertToBitmap()
-        python_height = python_png.GetHeight()
-        python_width = python_png.GetWidth()
-
-        wxpython_png = wx.Image('images/wxPython2.png',
-                                wx.BITMAP_TYPE_PNG).ConvertToBitmap()
-        wxpython_height = wxpython_png.GetHeight()
-        wxpython_width = wxpython_png.GetWidth()
-
-        h_margin = (WIDTH_CONSOLE - wxpython_width - python_width) / 3
-
-        png_height = max(python_height, wxpython_height) + V_MARGIN
-
-        v_margin = (png_height - python_height)/2
-        python_ypos = HEIGHT_CONSOLE - png_height + v_margin
-        v_margin = (png_height - wxpython_height)/2
-        wxpython_ypos = HEIGHT_CONSOLE -  png_height + v_margin
-
-        wx.StaticBitmap(self.panel_Console, -1, python_png,
-                        pos=(h_margin, python_ypos))
-        wx.StaticBitmap(self.panel_Console, -1, wxpython_png,
-                        pos=(python_width + 2*h_margin, wxpython_ypos))
-
-        self.png_height = png_height
 
         led_off = wx.Image('images/led_off.png',
                            wx.BITMAP_TYPE_PNG).ConvertToBitmap()
@@ -205,39 +183,28 @@ class MyFrame(wx.Frame):
                                        size=(WIDTH_CONSOLE-2*CTL_MARGIN, 25))
         y_pos += 15
 
-        dc = wx.PaintDC(self.panel_Console)
-        dc.SetPen(wx.Pen('black', 1))
-        dc.DrawLine(0, HEIGHT_CONSOLE - self.png_height,
-                    WIDTH_CONSOLE-1, HEIGHT_CONSOLE - self.png_height)
-
-        for (x, y) in self.led_ac.ticks:
-            dc.DrawLine(x, y, x, y+5)
-        first = self.led_ac.ticks[0]
-        last = self.led_ac.ticks[-1]
-        (x1, y1) = first
-        (x2, y2) = last
-        dc.DrawLine(x1, y2+5, x2, y2+5)
-
-        for (x, y) in self.led_pc.ticks:
-            dc.DrawLine(x, y, x, y+5)
-        first = self.led_pc.ticks[0]
-        last = self.led_pc.ticks[-1]
-        (x1, y1) = first
-        (x2, y2) = last
-        dc.DrawLine(x1, y2+5, x2, y2+5)
-
         self.y_offset = 0
         self.y_sign = +1
+
+        #self.panel_Screen.Bind(wx.EVT_PAINT, self.on_paint) 
+        #self.panel_Console.Bind(wx.EVT_PAINT, self.on_paint) 
+        self.Bind(wx.EVT_PAINT, self.on_paint) 
+        self.Bind(wx.EVT_CLOSE, self.onClose)
 
         self.Fit() 
 
         self.queue = collections.deque()
-        self.draw_list = ((100,100,924,924),)
+        self.draw_list = []
         self.worker = ImlacCpuThread(self, self.queue)
 
 
     def on_paint(self, event=None):
         global count
+
+        # draw the LED ticks
+        dc = wx.PaintDC(self.panel_Console)
+        self.led_ac.paint(dc)
+        self.led_pc.paint(dc)
 
         # establish the painting surface
         dc = wx.PaintDC(self.panel_Screen)
@@ -252,29 +219,15 @@ class MyFrame(wx.Frame):
         for args in self.draw_list:
             dc.DrawLine(*args)
 
-        dc = wx.PaintDC(self.panel_Console)
-        dc.SetPen(wx.Pen('black', 1))
-        dc.DrawLine(0, HEIGHT_CONSOLE - self.png_height,
-                    WIDTH_CONSOLE-1, HEIGHT_CONSOLE - self.png_height)
-
-        for (x, y) in self.led_ac.ticks:
-            dc.DrawLine(x, y, x, y+5)
-        first = self.led_ac.ticks[0]
-        last = self.led_ac.ticks[-1]
-        (x1, y1) = first
-        (x2, y2) = last
-        dc.DrawLine(x1, y2+5, x2, y2+5)
-
-        for (x, y) in self.led_pc.ticks:
-            dc.DrawLine(x, y, x, y+5)
-        first = self.led_pc.ticks[0]
-        last = self.led_pc.ticks[-1]
-        (x1, y1) = first
-        (x2, y2) = last
-        dc.DrawLine(x1, y2+5, x2, y2+5)
-
         count += 1
         self.led_ac.set_value(count)
+        print('count=%d' % count)
+
+    def onClose(self, event):
+        self.worker.abort()
+        print('after abort()')
+        self.Destroy()
+        print('after Destroy()')
 
 
 # test it ...
